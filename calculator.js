c4d4fb3dfef4d2e8780a728e69af43823f6fb656
Rowan Strang <rowanstrang@gmail.com>
2024-07-10 17:49:27 +1200

    calculator page ready- fist draft

--- calculator.js
+++ calculator.js
@@ -2,13 +2,13 @@
 
 let pricingPieces = {
   nextDayFilm: {
-    state: true,
+    state: false,
     cost: 3400,
     name: 'The Next Day Film',
     button: document.getElementById('nextDayFilm'),
   },
   heirloomFilm: {
-    state: false,
+    state: true,
     cost: 4500,
     name: 'The Heirloom Film',
     button: document.getElementById('heirloomFilm'),
@@ -44,146 +44,54 @@ let cost = 0
 let quoteList = document.querySelector('.quote ul')
 let quoteTotal = document.getElementById('total')
 
-function updateViewNew() {
+// function calculateTotal (){
+//   var quoteTotal=0
+//   for (const key in pricingPieces){
+//     if
+//   }
+// }
+
+function updateView() {
+  var totalPrice = 0
   quoteList.innerHTML = ''
   quoteTotal.innerHTML = ''
   for (const key in pricingPieces) {
     if (pricingPieces[key].state) {
-      // console.log('happy boy')
       pricingPieces[key].button.classList.add('active')
-      quoteList.innerHTML +=
+      ;(quoteList.innerHTML +=
         '<li>' +
         pricingPieces[key].name +
         ': $' +
         pricingPieces[key].cost +
-        '</li>'
+        '</li>'),
+        (totalPrice = totalPrice + pricingPieces[key].cost)
     } else {
       pricingPieces[key].button.classList.remove('active')
     }
   }
+  quoteTotal.innerHTML = 'Total: $' + totalPrice
 }
 
-// function addSomeItems() {
-//   quoteList.innerHTML += '<li>Next Day Film: $3400</li>'
-//   quoteList.innerHTML += '<li>Heirloom Film: $3400</li>'
-//   quoteList.innerHTML += '<li>Something New: $3400</li>'
-// }
-updateViewNew()
-// addSomeItems()
+updateView()
+readyForMoreClicks()
 
-// console.log(
-//   'next day film: ' +
-//     pricingPieces.nextDayFilm.state +
-//     '   Heirloom Film: ' +
-//     pricingPieces.heirloomFilm.state
-// )
-
-// let nextDayFilm = {
-//   state: true,
-//   cost: 3400,
-//   name: 'The Next Day Film',
-//   button: document.getElementById('nextDayFilm'),
-// }
-// let heirloomFilm = {
-//   state: false,
-//   cost: 4500,
-//   name: 'The Heirloom Film',
-//   button: document.getElementById('heirloomFilm'),
-// }
-// let spokenMoments = {
-//   state: false,
-//   cost: 1000,
-//   name: 'and Spoken Moments',
-//   button: document.getElementById('spokenMoments'),
-// }
-// let photography = {
-//   state: false,
-//   cost: 1000,
-//   name: 'with Photogrpahy',
-//   button: document.getElementById('photography'),
-// }
-// let liveStream = {
-//   state: true,
-//   cost: 50,
-//   name: 'plus a Ceremony Livestream',
-//   button: document.getElementById('liveStream'),
-// }
-
-// function updateView() {
-//   if (nextDayFilm.state) {
-//     nextDayFilm.button.classList.add('active')
-//     heirloomFilm.button.classList.remove('active')
-//   } else {
-//     heirloomFilm.button.classList.add('active')
-//     nextDayFilm.button.classList.remove('active')
-//   }
-//   if (spokenMoments.state) {
-//     spokenMoments.button.classList.add('active')
-//   }
-//   if (photography.state) photography.button.classList.add('active')
-//   if (liveStream.state) liveStream.button.classList.add('active')
-//   quoteList.innerHTML = ''
-// }
-
-// updateView()
-//list for button clicks. log button clicked
-
-// function logClick(buttonClicked) {
-//   if (choices[progress] == buttonClicked) {
-//     choices.splice(progress, 1)
-//   } else {
-//     choices[progress] = buttonClicked
-//   }
-//   console.log(choices)
-// }
-// function updateViewOLD() {
-//   if (questions.length === progress) {
-//     options.innerHTML = ''
-//     continueButton.innerHTML = ''
-//     question.innerHTML = ''
-//   } else {
-//     question.textContent = questions[progress]
-//     options.innerHTML = ''
-//     continueButton.innerHTML = ''
-//     for (let i = 0; i < answers[progress].length; i++) {
-//       var button = document.createElement('button')
-//       button.textContent = answers[progress][i]
-//       button.className = 'button-30'
-//       if (choices[progress] === i) {
-//         button.classList.toggle('active')
-//       }
-//       options.appendChild(button)
-//     }
-//     if (choices[progress] != null) {
-//       var button = document.createElement('button')
-//       button.textContent = 'Continue'
-//       button.className = 'button-continue'
-//       continueButton.appendChild(button)
-//     }
-//     readyForMoreClicks()
-//   }
-// }
-
-// function readyForMoreClicks() {
-//   var buttons = document.querySelectorAll('.button-30') // Get all buttons with the class '.button-30'
-//   buttons.forEach(function (button, index) {
-//     button.removeEventListener('click', handleButtonClick)
-//     button.addEventListener('click', handleButtonClick)
-//     function handleButtonClick() {
-//       logClick(index)
-//       updateView()
-//     }
-//   })
-//   var continueButton = document.querySelectorAll('.button-continue')
-//   continueButton.forEach(function (button) {
-//     button.removeEventListener('click', continueButtonClick)
-//     button.addEventListener('click', continueButtonClick)
-//     function continueButtonClick() {
-//       progress++
-//       updateView()
-//     }
-//   })
-// }
+function readyForMoreClicks() {
+  var buttons = document.querySelectorAll('.button-30') // Get all buttons with the class '.button-30'
+  buttons.forEach(function (button) {
+    button.removeEventListener('click', handleButtonClick)
+    button.addEventListener('click', handleButtonClick)
+    function handleButtonClick() {
+      // console.log(button.id)
+      if (button.id === 'heirloomFilm' || button.id === 'nextDayFilm') {
+        pricingPieces.heirloomFilm.state = !pricingPieces.heirloomFilm.state
+        pricingPieces.nextDayFilm.state = !pricingPieces.nextDayFilm.state
+      } else {
+        pricingPieces[button.id].state = !pricingPieces[button.id].state
+      }
+      updateView()
+    }
+  })
+}
 
 // function ConsLog() {
 //   console.log('this works when called')
